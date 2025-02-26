ce0c2c00ea5ee42cb00ce239c9391132f91916c9
iAbhikarsh <iabhikarshsingh@gmail.com>
2025-02-26 15:43:08 +0530

    commenting import

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const totalBudget = 693000;
 let totalSpent = 0;
 let expenses = [];
 
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
+//  // Import the functions you need from the SDKs you need
+//  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+//  // TODO: Add SDKs for Firebase products that you want to use
+//  // https://firebase.google.com/docs/web/setup#available-libraries
 
  // Your web app's Firebase configuration
  const firebaseConfig = {
